b456e5dd571bc493a844a88c54dc62d9655843ce
jonathanc816 <55709123+jonathanc816@users.noreply.github.com>
2022-11-21 00:11:04 -0800

    Successfully deploy the website via github

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './Home';
 import BullsAndCowspg from './BullsAndCows';
 import GuessNumpg from './GuessNum';
